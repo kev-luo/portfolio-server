47c6bb2ffcee362412e59604ff17484074169557
Kevin Luo <kvn.luo@gmail.com>
2020-11-18 19:29:20 -0500

    finish project mongoose model

--- models/Project.js
+++ models/Project.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-  
+  name: String,
+  description: String,
+  repo: String,
+  deployed: String,
 })
 
 module.exports = mongoose.model("Project", projectSchema);
--- models/Skill.js
+++ models/Skill.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const skillSchema = new mongoose.Schema({
   name: String,
+  
 })
 
 module.exports = mongoose.model("Skill", skillSchema);
